dd489024bac6638e61dcd18e22377103cac2382b
fontlck <font.uddi@gmail.com>
2025-09-21 22:07:53 +0700

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,15 +1,1 @@
-// firebase.js — config (photobooth-booking)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-export const firebaseConfig = {
-  apiKey: "AIzaSyCNVv93FykHDyNbpCldFt6DcdR2uHrx0Yo",
-  authDomain: "photobooth-booking.firebaseapp.com",
-  projectId: "photobooth-booking",
-  storageBucket: "photobooth-booking.appspot.com",
-  messagingSenderId: "231205524281",
-  appId: "1:231205524281:web:322d26f5ac15da75d64ee8"
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+/* firebase.js with config */
